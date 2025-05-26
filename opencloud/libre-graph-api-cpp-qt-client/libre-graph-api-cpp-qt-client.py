793395465cf448d19ef18b5068db0e960676a814
Hannah von Reth <h.vonreth@opencloud.eu>
2025-05-26 17:25:48 +0200

    libre-graph-api-cpp-qt-client v1.0.6

--- opencloud/libre-graph-api-cpp-qt-client/libre-graph-api-cpp-qt-client.py
+++ opencloud/libre-graph-api-cpp-qt-client/libre-graph-api-cpp-qt-client.py
@@ -4,17 +4,16 @@ from Utils import CraftHash
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ["1.0.3", "1.0.4"]:
-            self.targets[ver] = f"https://github.com/owncloud/libre-graph-api-cpp-qt-client/archive/refs/tags/v{ver}.tar.gz"
+        for ver in ["1.0.6"]:
+            self.targets[ver] = f"https://github.com/opencloud-eu/libre-graph-api-cpp-qt-client/archive/refs/tags/v{ver}.tar.gz"
             self.targetConfigurePath[ver] = "client"
             self.targetInstSrc[ver] = f"libre-graph-api-cpp-qt-client-{ver}"
-        self.targetDigests["1.0.3"] = (["17cd8b03f5fca97d9944701f183dfd44d88c3a00a01ed0395aa925768551d16c"], CraftHash.HashAlgorithm.SHA256)
-        self.targetDigests["1.0.4"] = (["5504024235b8474eb053dad0051d33f3fcfd9aabdbde96f506a77df181fcd84d"], CraftHash.HashAlgorithm.SHA256)
+        self.targetDigests["1.0.6"] = (["02a8f5efc079d022faf7bc6da6c0a646dfee96ca4d1bd505d8a6f2f1a0ef6ebc"], CraftHash.HashAlgorithm.SHA256)
 
         self.svnTargets["main"] = f"https://github.com/opencloud-eu/libre-graph-api-cpp-qt-client.git|main|"
         self.targetConfigurePath["main"] = "client"
 
-        self.defaultTarget = "1.0.4"
+        self.defaultTarget = "1.0.6"
         self.description = "Libre Graph Cloud Collaboration API - Qt bindings"
 
     def setDependencies(self):
